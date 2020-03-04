b1e100d50f3143d63b9f01ebaef4654b8e8ade66
Kunal Attri <attrikunal16@gmail.com>
2020-03-05 01:51:47 +0530

    combinations code merged with permutations

---
+++ Permutations&Combinations.py
@@ -0,0 +1,19 @@
+from Basic_Functions import *
+
+print('''
+This code calculates no of possible permutations and combinations. Kindly provide data as n and r
+n: total no of items 
+r: required no. of items
+''')
+
+while True:
+    n = get_integer("n: ")
+    r = get_integer('r: ')
+    if n - r < 0:
+        print('Invalid input, since n can never be less than r')
+    else:
+        perm = int(factorial(n) / factorial(n - r))
+        comb = int(factorial(n) / (factorial(r) * factorial(n - r)))
+        print(f'Possible permutations = {perm}')
+        print(f'Possible Combinations = {comb}')
+    print('')
